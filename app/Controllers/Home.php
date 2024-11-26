89d8a6377e1dd6d80fd323f2fa37476c7f578bce
caleblent <caleb@stcroixwebagency.com>
2024-11-25 20:56:30 -0600

    reset password link now if magic email link used to login

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,13 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
+        if (session("magicLogin")) {
+            return redirect()->to("set-password")
+                            ->with("message", "Please update your password");
+        }
+
         return view("Home/index");
     }
 
