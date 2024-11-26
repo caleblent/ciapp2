aa66fc56cf256724a414e75d5ce5fcdb82f0bce6
caleblent <caleb@stcroixwebagency.com>
2024-11-25 18:01:40 -0600

    add 'log in/out' message on Home based on if user is logged in or not

--- app/Views/Home/index.php
+++ app/Views/Home/index.php
@@ -4,4 +4,16 @@
 
 <?= $this->section("content") ?>
     <h1>Welcome to the Home page!</h1>
+
+    <a href="<?= url_to("articles") ?>">See articles</a>
+
+    <?php if (auth()->loggedIn()): ?>
+
+        <a href="<?= url_to("logout") ?>">Log out</a>
+
+    <?php else: ?>
+
+        <a href="<?= url_to("login") ?>">Log in</a>
+
+    <?php endif; ?>
 <?= $this->endSection() ?>
