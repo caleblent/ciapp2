be0be68ea6042522b6672d579287e0d196b9e31d
caleblent <caleb@stcroixwebagency.com>
2024-11-25 19:17:55 -0600

    added sendTestEmail() func

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,20 @@ class Home extends BaseController
     {
         return view("Home/index");
     }
+
+    private function sendTestEmail()
+    {
+        $email = \Config\Services::email();
+
+        $email->setTo("caleb@stcroixwebagency.com");
+
+        $email->setSubject("Test Email");
+        $email->setMessage("Hello from <i>CodeIgniter</i>");
+
+        if ($email->send()) {
+            echo "Email sent";
+        } else {
+            echo "Email NOT sent";
+        }
+    }
 }
